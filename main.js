d3b177811c71b9acb974dae98890dd7a6bf54838
niccolosantelli <niccolo_santelli@live.it>
2020-11-26 15:34:23 +0100

    aggiornamento
    rifatto meglio

--- main.js
+++ main.js
@@ -1,43 +1,26 @@
 /*$("div.hamburger-menu").show();*/
 
-var close = $("i.fa-bars");
-var x = $("i.fa-times")
-
 /*var open = false;
 close.click(function () {
-    if(open) {
-        $('div.hamburger-menu').show();
-        open = false;
+if(open) {
+$('div.hamburger-menu').show();
+open = false;
 
-    } else {
-        $('div.hamburger-menu').hide();
-        open = true;
-    }
+} else {
+$('div.hamburger-menu').hide();
+open = true;
+}
 
 });*/
 
-var open = false;
-close.click(function () {
-     if (open) {
-        $("div.hamburger-menu").hide();
-        open = false;
+var hb = $("i.fa-bars");
+var x = $("i.fa-times")
 
-     } else {
+hb.click(function () {
        $("div.hamburger-menu").show();
-       open = true
-     }
-
 })
 
-var chiuso = false;
+
 x.click(function (){
-  if (open) {
      $("div.hamburger-menu").hide();
-     open = false;
-
-  } else {
-    $("div.hamburger-menu").show();
-    open = true
-  }
-
 })
